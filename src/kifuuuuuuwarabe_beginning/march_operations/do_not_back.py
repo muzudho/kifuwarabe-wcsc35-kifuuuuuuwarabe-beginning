7ee091354a9f3bf573992191eee0a4f1efff3762
muzudho <muzudho1@gmail.com>
2025-03-14 23:41:31 +0900

    Bug Fix: `not in` to `in`.

--- src/kifuuuuuuwarabe_beginning/march_operations/do_not_back.py
+++ src/kifuuuuuuwarabe_beginning/march_operations/do_not_back.py
@@ -19,6 +19,7 @@ class DoNotBack(MatchOperation):
                 label       = '戻るな',
                 config_doc  = config_doc)
 
+        print(f'★ ＤoNotBack: back_board 生成')
         # 元の位置。該当がなければナンです。
         # NOTE 自分が指し手を送信した手しか記憶していません。
         self._back_board = [None] * constants.BOARD_AREA
@@ -31,19 +32,28 @@ class DoNotBack(MatchOperation):
         ban = Ban(table)
         cmp = Comparison(table)
 
+        is_drop = cshogi.move_is_drop(move)
         src_sq_obj = Square(cshogi.move_from(move))
         dst_sq_obj = Square(cshogi.move_to(move))
 
-        # 動かす駒は、現在のマスに移動する前はどこに居たか調べます。
-        try:
-            back_sq = self._back_board[src_sq_obj.sq]
-        except IndexError:
-            print(f'ERROR: {src_sq_obj.sq=}', file=sys.stderr)
-            raise
+        if is_drop: # 打のとき
+            back_sq = constants.PIECE_STAND_SQ
+        
+        else:
+            # 動かす駒は、現在のマスに移動する前はどこに居たか調べます。
+            try:
+                back_sq = self._back_board[src_sq_obj.sq]
+            except:
+                # NOTE 角打だと 89 が返ってくる？
+                print(f'ERROR1: {src_sq_obj.sq=}')
+                #print(f'ERROR2: {src_sq_obj.sq=}', file=sys.stderr)
+                raise
 
         if back_sq in [None, constants.PIECE_STAND_SQ]:
             # 動いていない、または、駒台にあったなら、対象外
             return constants.mind.NOT_IN_THIS_CASE
+
+        print(f'★ ＤoNotBack: {Helper.sq_to_masu(src_sq_obj.sq)=} の前位置は {Helper.sq_to_masu(back_sq)=}。')
 
         # 元居た位置に戻る手は、意志無し。
         if dst_sq_obj.sq == back_sq:
@@ -63,7 +73,7 @@ class DoNotBack(MatchOperation):
             is_drop = cshogi.move_is_drop(move)
 
             # 戻れない駒は対象外。
-            if cshogi.move_from_piece_type(move) not in [cshogi.KNIGHT, cshogi.LANCE, cshogi.PAWN]:
+            if cshogi.move_from_piece_type(move) in [cshogi.KNIGHT, cshogi.LANCE, cshogi.PAWN]:
                 return
 
             # 移動元のマス番号
@@ -75,4 +85,4 @@ class DoNotBack(MatchOperation):
             # 記憶
             self._back_board[dst_sq_obj.sq] = src_sq
             self._back_board[src_sq] = None
-            print(f'★ DoNotBack: {dst_sq_obj.sq=} に {src_sq=} を記憶', file=sys.stderr)
+            print(f'★ ＤoNotBack: {Helper.sq_to_masu(dst_sq_obj.sq)=} に前位置 {Helper.sq_to_masu(src_sq)=} を記憶。')
