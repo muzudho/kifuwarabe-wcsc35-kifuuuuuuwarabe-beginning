66aee5c56f4aea14edc58e3a225ce0545ee02f0e
muzudho <muzudho1@gmail.com>
2025-04-16 21:56:54 +0900

    WIP 駒得計算変更中

--- src/kifuuuuuuwarabe_beginning/models/layer_o3o0/quiescence_search_for_scramble_model.py
+++ src/kifuuuuuuwarabe_beginning/models/layer_o3o0/quiescence_search_for_scramble_model.py
@@ -150,8 +150,7 @@ class QuiescenceSearchForScrambleModel():
         case_6t_hint_list = []
         case_6f = 0
         case_6f_hint_list = []
-        case_8at = 0
-        case_8af = 0
+        case_8a = 0
         case_8b = 0
         case_8c = 0
         case_8d = 0
@@ -235,18 +234,9 @@ class QuiescenceSearchForScrambleModel():
 
             this_branch_value_on_earth = child_plot_model.peek_piece_exchange_value_on_earth + piece_exchange_value_on_earth
 
-            # この枝が長兄（兄枝がまだ無い）なら。
+            # この枝が長兄なら採用。
             if best_old_sibling_plot_model_in_children is None:
-
-                e1 = ptolemaic_theory_model.swap(0, this_branch_value_on_earth)
-                # この枝が０点以上なら最善手（［Not Bad］）として回答。この最善手が最終的に即採用されるというわけではない。
-                its_update_best = (e1[0] <= e1[1])
-
-                if its_update_best:
-                    case_8at += 1
-                else:
-                    case_8af += 1
-                #case_8_hint_list.append(f"{old_sibling_value=} < {this_branch_value=}")
+                case_8a += 1
             
             # 兄枝は有るが、兄枝に［手］が無い場合。（［宣言］の直前とか、最大深さとか）
             elif best_old_sibling_plot_model_in_children.is_empty_moves():
@@ -361,7 +351,7 @@ class QuiescenceSearchForScrambleModel():
                     declaration                             = constants.declaration.NO_CANDIDATES,  # 有力な候補手無し。
                     is_mate_in_1_move                       = False,
                     cutoff_reason                           = cutoff_reason.NO_MOVES,
-                    hint                                    = f"指したい{self._search_model.max_depth - depth + 1}階の手無し,敵={is_mars},move数={len(legal_move_list)},{case_1=},{case_2=},{case_4=},{case_5=},{case_6t=},({'_'.join(case_6t_hint_list)}),{case_6f=},({'_'.join(case_6f_hint_list)}),{case_8at=},{case_8af=},{case_8b=},{case_8c=},{case_8d=},{case_8e=}")
+                    hint                                    = f"指したい{self._search_model.max_depth - depth + 1}階の手無し,敵={is_mars},move数={len(legal_move_list)},{case_1=},{case_2=},{case_4=},{case_5=},{case_6t=},({'_'.join(case_6t_hint_list)}),{case_6f=},({'_'.join(case_6f_hint_list)}),{case_8a=},{case_8a=},{case_8b=},{case_8c=},{case_8d=},{case_8e=}")
 
         # 今回の手を付け加える。
         best_old_sibling_plot_model_in_children.append_move(
