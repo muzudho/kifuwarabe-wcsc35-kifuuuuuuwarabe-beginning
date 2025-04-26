77af1531a4e855d2a1accb142431f9717a702a95
muzudho <muzudho1@gmail.com>
2025-04-26 22:39:32 +0900

    Enbug Fix: BPM

--- src/kifuuuuuuwarabe_beginning/models/layer_o2o0/backwards_plot_model.py
+++ src/kifuuuuuuwarabe_beginning/models/layer_o2o0/backwards_plot_model.py
@@ -37,7 +37,7 @@ class BackwardsPlotModel(): # TODO Rename PathFromLeaf
         return value
 
 
-    def __init__(self, is_mars_at_out_of_termination, is_gote_at_out_of_termination, out_of_termination, hint, move_list = [], cap_list = [], list_of_accumulate_exchange_value_on_earth = []):
+    def __init__(self, is_mars_at_out_of_termination, is_gote_at_out_of_termination, out_of_termination, hint):
         """初期化。
 
         Parameters
@@ -48,16 +48,39 @@ class BackwardsPlotModel(): # TODO Rename PathFromLeaf
             ［葉局面］＝［終端外］手番は後手か。
         out_of_termination : int
             ［終端外］
-        hint_list : list<str>
+        hint : str
             デバッグ用文字列
         """
         self._is_mars_at_out_of_termination = is_mars_at_out_of_termination
         self._is_gote_at_out_of_termination = is_gote_at_out_of_termination
-        self._out_of_termination            = out_of_termination
-        self._hint_list                     = [hint]
-        self._move_list                     = move_list
-        self._cap_list                      = cap_list
-        self._list_of_accumulate_exchange_value_on_earth = list_of_accumulate_exchange_value_on_earth   # 地球から見た、取った駒の交換値。
+        self._out_of_termination = out_of_termination
+        self._move_list = []
+        self._cap_list = []
+        self._list_of_accumulate_exchange_value_on_earth = []   # 地球から見た、取った駒の交換値。
+        self._hint_list = [hint]
+
+
+    # def __init__(self, is_mars_at_out_of_termination, is_gote_at_out_of_termination, out_of_termination, hint, move_list = [], cap_list = [], list_of_accumulate_exchange_value_on_earth = []):
+    #     """初期化。
+
+    #     Parameters
+    #     ----------
+    #     is_mars_at_out_of_termination : bool
+    #         ［葉局面］＝［終端外］手番は対戦相手か。
+    #     is_gote_at_out_of_termination : bool
+    #         ［葉局面］＝［終端外］手番は後手か。
+    #     out_of_termination : int
+    #         ［終端外］
+    #     hint_list : list<str>
+    #         デバッグ用文字列
+    #     """
+    #     self._is_mars_at_out_of_termination = is_mars_at_out_of_termination
+    #     self._is_gote_at_out_of_termination = is_gote_at_out_of_termination
+    #     self._out_of_termination            = out_of_termination
+    #     self._hint_list                     = [hint]
+    #     self._move_list                     = move_list
+    #     self._cap_list                      = cap_list
+    #     self._list_of_accumulate_exchange_value_on_earth = list_of_accumulate_exchange_value_on_earth   # 地球から見た、取った駒の交換値。
 
 
     @property
@@ -268,12 +291,12 @@ class BackwardsPlotModel(): # TODO Rename PathFromLeaf
         return f"{len(self._move_list)=} {len(self._cap_list)=} {len(self._list_of_accumulate_exchange_value_on_earth)=}"
 
 
-    def copy_bpm(self):
-        return BackwardsPlotModel(
-                is_mars_at_out_of_termination   = self._is_mars_at_out_of_termination,
-                is_gote_at_out_of_termination   = self._is_gote_at_out_of_termination,
-                out_of_termination              = self._out_of_termination,
-                hint                            = list(self._hint),
-                move_list                       = list(self._move_list),
-                cap_list                        = list(self._cap_list),
-                list_of_accumulate_exchange_value_on_earth = list(self._list_of_accumulate_exchange_value_on_earth))
+    # def copy_bpm(self):
+    #     return BackwardsPlotModel(
+    #             is_mars_at_out_of_termination   = self._is_mars_at_out_of_termination,
+    #             is_gote_at_out_of_termination   = self._is_gote_at_out_of_termination,
+    #             out_of_termination              = self._out_of_termination,
+    #             hint                            = list(self._hint),
+    #             move_list                       = list(self._move_list),
+    #             cap_list                        = list(self._cap_list),
+    #             list_of_accumulate_exchange_value_on_earth = list(self._list_of_accumulate_exchange_value_on_earth))
