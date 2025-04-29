c1dce468f0ba6f9e369a42f3ad034ca9430decce
muzudho <muzudho1@gmail.com>
2025-04-29 21:21:36 +0900

    Bug Fix:

--- kifuuuuuuwarabe_beginning.bat
+++ kifuuuuuuwarabe_beginning.bat
@@ -1,4 +1,5 @@
-rem set PATH=%%PATH%%;C:\Users\muzud\anaconda3
+rem Configure the build path if necessary.
+set PATH=%%PATH%%;C:\Users\muzud\anaconda3
 
+rem The command name is either python or py.
 python main.py
-rem py main.py
